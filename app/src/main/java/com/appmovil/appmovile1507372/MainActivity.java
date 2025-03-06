3349721174a4fdf184767e9fd8455f28ca923d28
agurto25 <agurto2506@gmail.com>
2025-03-06 18:56:00 -0500

    firebase 06/03/25

--- app/src/main/java/com/appmovil/appmovile1507372/MainActivity.java
+++ app/src/main/java/com/appmovil/appmovile1507372/MainActivity.java
@@ -2,16 +2,24 @@ package com.appmovil.appmovile1507372;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -47,8 +55,26 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent con = new Intent(MainActivity.this,Pantalla_Inicio.class);
-                startActivity(con);0
+                String emaillogin = txtEmail.getText().toString();
+                String passlogin = txtPassLogin.getText().toString();
+
+                if (TextUtils.isEmpty(emaillogin)||TextUtils.isEmpty(passlogin)){
+                    Toast.makeText(MainActivity.this,"Los campos son obligatorios", Toast.LENGTH_SHORT).show();
+                }else {
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(emaillogin,passlogin).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                Intent con = new Intent(MainActivity.this,Pantalla_Inicio.class);
+                                startActivity(con);
+                                finish();
+                            }else{
+                                Toast.makeText(MainActivity.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
             }
         });
 
--- app/src/main/java/com/appmovil/appmovile1507372/Register.java
+++ app/src/main/java/com/appmovil/appmovile1507372/Register.java
@@ -2,15 +2,28 @@ package com.appmovil.appmovile1507372;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -40,5 +53,37 @@ public class Register extends AppCompatActivity {
             }
         });
 
+        btnRegistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nombreusuario = txtApellidoNombre.getText().toString();
+                String telefonousuario = txtTelefono.getText().toString();
+                String emailusuario = txtCorreo.getText().toString();
+                String passusuario = txtContraseña.getText().toString();
+
+                if (TextUtils.isEmpty(nombreusuario)||TextUtils.isEmpty(telefonousuario)||TextUtils.isEmpty(emailusuario)||TextUtils.isEmpty(passusuario)){
+                    Toast.makeText(Register.this,"Todos los campos son obligatorios",Toast.LENGTH_SHORT).show();
+                }else{
+                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailusuario,passusuario).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                String UserId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                                Map<String,String> user = new HashMap<>();
+                                user.put("username",nombreusuario);
+                                user.put("Telefono", telefonousuario);
+                                user.put("email", emailusuario);
+
+                                FirebaseDatabase.getInstance().getReference().child("usuarios").child(UserId).setValue(user);
+                                Toast.makeText(Register.this,"Registro Exitoso",Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                Toast.makeText(Register.this,task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
 }
